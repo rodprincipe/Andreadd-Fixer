9a7186b800a80964680f7cfa26accfb89c05cba2
rodprincipe <rodprincipe@users.noreply.github.com>
2019-06-10 16:45:59 +0200

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -22,6 +22,6 @@ $('.plussbullets a').each(function(i){
     var urlsplit = urlb.split("/");
     var nome = urlsplit[urlsplit.length-1];
     $(this).attr('href',urlb);
-    $(this).attr('download',nome);
-
+    //uncomment next line to direct download
+    //$(this).attr('download',nome);       
 });
